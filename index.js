a63010dbde67c8b7a9fe0e6b86d627556494c9c4
David Essel <davidessel@Davids-MacBook-Pro.local>
2025-02-15 01:09:09 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function changeStyle() {
+    let body = document.getElementById('body');
+    let background = document.getElementById('background').value;
+    let color = document.getElementById('color').value;
+  
+    body.style.background = background;
+    body.style.color = color;
+
+
+
+  };
